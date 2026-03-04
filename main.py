fed79436cd35c2f25e12f17540ca48e1aa3fb831
m200294 <wolfman2030@gmail.com>
2026-03-04 04:52:38 +0200

    working event loop, and close

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
 from constants import *
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            screen.fill("black")
+            pygame.display.flip()
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
+
