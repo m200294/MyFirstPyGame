68ce1d16128fb912e93cd8a412caf258b90d4ca2
m200294 <wolfman2030@gmail.com>
2026-03-04 03:06:08 +0200

    logger function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+from constants import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
